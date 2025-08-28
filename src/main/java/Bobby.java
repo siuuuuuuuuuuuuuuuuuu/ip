747e0f612ff068a0499737c157d9ed1ddf478c2a
marcus <marcus.1020@hotmail.com>
2025-08-29 01:24:21 +0800

    Level-1

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Bobby is a simple chatbot that echoes user input until the user types "bye".
+ */
 public class Bobby {
+    private static final String EXIT_COMMAND = "bye";
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Bobby\nHow can I help you?\n\nBye.Hope to see you again soon!");
+        System.out.println("Hello! I'm Bobby\nHow can I help you?");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase(EXIT_COMMAND)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+        scanner.close();
     }
 }
